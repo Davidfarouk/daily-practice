6da916a6bdcb46a3766b638e08425878c5c96997
Davidfarouk <davidfarouk95@gmail.com>
2024-06-14 12:00:00 +0200

    Update algorithms.py

--- algorithms.py
+++ algorithms.py
@@ -1,5 +1,11 @@
-def search(arr, x):
-    for i, v in enumerate(arr):
-        if v == x:
-            return i
+def binary_search(arr, x):
+    left, right = 0, len(arr)-1
+    while left <= right:
+        mid = (left+right)//2
+        if arr[mid] == x:
+            return mid
+        elif arr[mid] < x:
+            left = mid+1
+        else:
+            right = mid-1
     return -1
